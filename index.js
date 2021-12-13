1fd5cb0de493a05fcc6faab5d7231cf0e2e9643e
Jonathon LeVitre <jonathonlevitre@jonathons-mbp.lan>
2021-12-13 16:40:55 -0700

    more stuff

---
+++ index.js
@@ -0,0 +1,27 @@
+function solution(a) {
+    let count = 0
+for(let i =0 ; i < a.length; i++){
+    let x = a[i-1]
+    if(!x){
+        x = 0
+    }
+    let y = a[i+1]
+    if(!y){
+        y = 0
+    }
+    if(a[i] === (x + y)/2){
+        count++
+    }
+}
+return count
+}
+
+function solution(a, x) {
+    let result = []
+    for(let i =0; i< a.length; i++){
+        if(a[i]!== x){
+            result.push(a[i])
+        }
+    }
+    return result
+    }
